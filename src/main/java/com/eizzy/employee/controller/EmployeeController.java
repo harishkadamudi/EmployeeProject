4b6a45c63127e587fdd2201d9c94ab6919f565b7
Harish Kadamudi <harishyouk@gmail.com>
2018-10-06 11:08:47 +0530

    API to add search

--- src/main/java/com/eizzy/employee/controller/EmployeeController.java
+++ src/main/java/com/eizzy/employee/controller/EmployeeController.java
@@ -18,12 +18,22 @@ public class EmployeeController {
         this.employeeRepo = employeeRepo;
     }
 
+    /**
+     * API used to create a Employee
+     * @param employee
+     * @return ResponseEntity of Employee
+     */
     @PostMapping("/employees")
     public ResponseEntity<Optional<Employee>> createEmployee(@RequestBody Employee employee) {
         Employee save = employeeRepo.save(employee);
         return ResponseEntity.ok(java.util.Optional.ofNullable(save));
     }
 
+    /**
+     * API used to create bulk Employees
+     * @param employees
+     * @return return number of employees created.
+     */
     @PostMapping("/employees/bulk")
     public ResponseEntity<String> createBulkEmployee(@RequestBody List<Employee> employees) {
         Optional<List<Employee>> list = Optional.of(employeeRepo.saveAll(employees));
@@ -32,17 +42,33 @@ public class EmployeeController {
     }
 
 
+    /**
+     * API used to get/display all Employees
+     * @return list of employees
+     */
     @GetMapping("/employees")
     public ResponseEntity<List<Employee>> employees() {
         List<Employee> employees = employeeRepo.findAll();
         return ResponseEntity.ok(employees);
     }
 
+    /**
+     * API used to find employee based on employeeId
+     * @param employeeId
+     * @return if found return employee or else empty employee object
+     */
     @GetMapping("/employees/{employeeId:[0-9]+}")
     public ResponseEntity<Employee> employee(@PathVariable Integer employeeId) {
         return buildResponse(employeeRepo.findById(employeeId));
 
     }
+
+
+    /**
+     * API used to find employee based on employee firstname
+     * @param firstname
+     * @return if found return employee or else empty employee object
+     */
 
     @GetMapping("/employees/{firstname:[a-zA-Z]+}")
     public ResponseEntity<Employee> employeeByFirstName(@PathVariable String firstname) {
--- src/main/java/com/eizzy/employee/repo/EmployeeRepo.java
+++ src/main/java/com/eizzy/employee/repo/EmployeeRepo.java
@@ -2,9 +2,11 @@ package com.eizzy.employee.repo;
 
 import com.eizzy.employee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface EmployeeRepo extends JpaRepository<Employee,Integer>{
 
     Optional<Employee> findByFirstNameIgnoreCase(String firstName);
