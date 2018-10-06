46a7aeb3459986ed9a1b0e32466c65b0f63c723e
Harish Kadamudi <harishyouk@gmail.com>
2018-10-06 13:16:01 +0530

    added-unit and integration tests

--- src/test/java/com/eizzy/employee/EmployeeIntegrationTest.java
+++ src/test/java/com/eizzy/employee/EmployeeIntegrationTest.java
@@ -2,11 +2,13 @@ package com.eizzy.employee;
 
 import com.eizzy.employee.model.Employee;
 import com.eizzy.employee.repo.EmployeeRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.GsonTester;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
@@ -16,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,6 +36,7 @@ public class EmployeeIntegrationTest {
 
     HttpHeaders headers = new HttpHeaders();
 
+    GsonTester<Employee> gsonTester;
     @Autowired
     private MockMvc mvc;
 
@@ -42,9 +46,21 @@ public class EmployeeIntegrationTest {
     private EmployeeRepo employeeRepo;
 
     @Test
+    public void whenPostEmployee_200Response() throws Exception{
+
+        Employee employee = new Employee();
+        employee.setFirstName("Test FirstName");
+        employee.setLastName("Test LastName");
+
+        mvc.perform(post("/employees")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(asJsonString(employee)))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
     public void givenEmployees_whenGetEmployees_thenStatus200()
             throws Exception {
-
         Employee employee = new Employee();
         employee.setFirstName("Test FirstName");
         employee.setLastName("Test LastName");
@@ -70,5 +86,13 @@ public class EmployeeIntegrationTest {
 
     private String buildurl(String uri){
         return "http://localhost:"+port+uri;
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
